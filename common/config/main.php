82133ae4584f6f418a63f362ef159558754bd353
Лина <potter062@gmail.com>
2019-06-25 14:48:00 +0300

    Initial commit

---
+++ common/config/main.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'language' => 'ru-Ru',
+    'timeZone' => 'Europe/Kiev',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+];
---
+++ frontend/controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Лина
+ * Date: 24.06.2019
+ * Time: 13:44
+ */
+
+namespace frontend\controllers;
+
+
+class TestController
+{
+
+}
