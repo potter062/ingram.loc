b1ce57b98e94ff6041874b9e578af21ff33e5de6
Лина <potter062@gmail.com>
2019-06-29 16:39:43 +0300

    Profil step 1

---
+++ frontend/modules/user/models/SignupForm.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace frontend\modules\user\models;
+
+use yii\base\Model;
+use frontend\models\User;
+
+/**
+ * Signup form
+ */
+class SignupForm extends Model
+{
+
+    public $username;
+    public $email;
+    public $password;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['username', 'trim'],
+            ['username', 'required'],
+            ['username', 'string', 'min' => 2, 'max' => 20],
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => 255],
+            ['email', 'unique', 'targetClass' => '\frontend\models\User', 'message' => 'This email address has already been taken.'],
+            ['password', 'required'],
+            ['password', 'string', 'min' => 6],
+        ];
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return User|null the saved model or null if saving fails
+     */
+    public function signup()
+    {
+        if (!$this->validate()) {
+            return null;
+        }
+
+        $user = new User();
+        $user->username = $this->username;
+        $user->email = $this->email;
+        $user->setPassword($this->password);
+        $user->generateAuthKey();
+
+        return $user->save() ? $user : null;
+    }
+
+}
